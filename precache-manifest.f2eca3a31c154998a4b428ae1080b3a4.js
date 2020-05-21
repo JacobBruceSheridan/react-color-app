18febdaf90fefacb0dfbba1b8728d97b084dba36
JacobBruceSheridan <brucejac@sheridancollege.ca>
2020-05-21 14:17:08 -0400

    Updates

---
+++ precache-manifest.f2eca3a31c154998a4b428ae1080b3a4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4f3f107dfb4759341de284f8b6d1b91",
+    "url": "/react-color-app/index.html"
+  },
+  {
+    "revision": "5549135197e4b60183f8",
+    "url": "/react-color-app/static/css/2.7095f003.chunk.css"
+  },
+  {
+    "revision": "8d4514fe91c8260a5486",
+    "url": "/react-color-app/static/css/main.0da9c1e9.chunk.css"
+  },
+  {
+    "revision": "5549135197e4b60183f8",
+    "url": "/react-color-app/static/js/2.551fb3d4.chunk.js"
+  },
+  {
+    "revision": "fe2f19c5d5965d51615a596cf8d07fe9",
+    "url": "/react-color-app/static/js/2.551fb3d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d4514fe91c8260a5486",
+    "url": "/react-color-app/static/js/main.c909536d.chunk.js"
+  },
+  {
+    "revision": "a1e0dbe48b0a50544815",
+    "url": "/react-color-app/static/js/runtime-main.5fac602c.js"
+  },
+  {
+    "revision": "f668aae023ad85679abf277241ff0258",
+    "url": "/react-color-app/static/media/bg.f668aae0.svg"
+  }
+]);
